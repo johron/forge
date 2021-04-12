a0a2169935c63fea8fc202e8e7cf55b1867921d9
Johan Rönnkvist <johan.ronnkvist@gmail.ccom>
2021-04-12 21:37:35 +0200

    Add ImGui and ImGui-SFML

--- forge/forge-client/src/main.cpp
+++ forge/forge-client/src/main.cpp
@@ -1,21 +1,36 @@
 #include "SFML/Graphics.hpp"
+#include "imgui-SFML.h"
+#include "imgui.h"
 
 int main(int argc, char** argv) {
 	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	window.setFramerateLimit(60);
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
 
+	ImGui::SFML::Init(window);
+
+	sf::Clock deltaClock;
 	while (window.isOpen()) {
 		sf::Event event;
 		while (window.pollEvent(event)) {
+			ImGui::SFML::ProcessEvent(event);
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
 
+		ImGui::SFML::Update(window, deltaClock.restart());
+
+		ImGui::Begin("Hello, world!");
+		ImGui::Button("Look at this pretty button");
+		ImGui::End();
+
 		window.clear();
 		window.draw(shape);
+		ImGui::SFML::Render(window);
 		window.display();
 	}
 
+	ImGui::SFML::Shutdown();
 	return 0;
 }
